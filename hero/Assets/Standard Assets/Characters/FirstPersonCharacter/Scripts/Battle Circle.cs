c033986633be5f3042da4ab3eefd99c0efff9957
AIE\s181563 <ignislupus.wolfie@gmail.com>
2018-08-23 16:40:45 +0930

    23818

---
+++ hero/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Battle Circle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleCircle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Transform avoidEnemy;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.tag == "EnemyRadius")
+        {
+
+            avoidEnemy = other.transform;
+
+        }
+
+    }
+}
